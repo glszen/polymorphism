7b0f7a06cf9e534b0d5311d385171005b9f800cc
Gülşen <gulsenk48@hotmail.com>
2024-10-22 22:33:45 +0300

    Add project files.

---
+++ BaseGeometricShape.cs
@@ -0,0 +1,41 @@
+﻿    public class BaseGeometricShape
+    {
+
+    public int width;
+
+    public int height;
+
+        public virtual void calculateArea()
+        {
+            int area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+ 
+    public class Square : BaseGeometricShape
+    {
+        public override void calculateArea()
+        {
+            int area = width * height;
+            Console.WriteLine("Square area is: " + area );
+        }
+    }
+
+    public class Rectangle : BaseGeometricShape
+    {
+        public override void calculateArea()
+        {
+            int area = width * height;
+            Console.WriteLine("Reactangle area is: " + area);
+        }
+    }
+
+    public class rightTriangle: BaseGeometricShape
+    {
+        public override void calculateArea()
+        {
+            int area = (width * height) / 2;
+            Console.WriteLine("Right Triangle area is: " + area);
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿BaseGeometricShape squareArea = new Square() { height = 40, width = 40 };
+BaseGeometricShape reactangleArea = new Rectangle() { height = 40, width = 60 };
+BaseGeometricShape rightTriangleArea = new rightTriangle() { height = 40, width = 50 };
+
+squareArea.calculateArea();
+reactangleArea.calculateArea();
+rightTriangleArea.calculateArea();
